9ac529c0c7bb7c72d9815e7532d041c6bce8902e
Asus <tungktph27526@fpt.edu.vn>
2023-01-30 21:46:49 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import {useState} from 'react';
 
 export default function App() {
+ 
+  const[stState, setStState] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView>
+        <View style={styles.container}>
+      <Text style={styles.text}>Kieu Thanh Tung</Text>
+      <Text style={styles.text}>PH27526</Text>
+      
+      <Button 
+          title={`${stState ? 'an': 'them moi'}`}
+          onPress ={() => setStState(!stState)}
+      />
+      {stState?<>
+          <TextInput placeholder='ten'style={styles.boders}/>
+          <TextInput placeholder='ma'style={styles.boders}/>
+          <TextInput placeholder='dia chi'style={styles.boders}/>
+          <TextInput placeholder='sdt'style={styles.boders}/>
+          <Button  
+          title='luu'
+          />
+          <Button
+          title='huy'
+          />
+          </>
+          : null
+      }
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
+    
   );
 }
 
@@ -16,5 +42,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: "black",
+    fontSize: 20,
+  },
+  boders:{
+    width: 200,
+    height: 54,
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
   },
 });
